cb7e9b419d66c5e3075c6c1ce4e8a4f05d32480c
turtleshow247 <143399512+biggads@users.noreply.github.com>
2023-10-18 00:32:27 +0545

    up

--- public/wp-content/plugins/magical-posts-display/assets/js/topic.js
+++
@@ -1,9 +0,0 @@
-;(function($){
-	$(document).ready(function(){
-    	$('.mg-dismiss').on('click',function(){
-            var url = new URL(location.href);
-            url.searchParams.append('mghide',1);
-            location.href= url;
-        });
-	});
-})(jQuery);
